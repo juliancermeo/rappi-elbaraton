efddffb477a53c0d077a9385414a5fde1c37f7f5
juliancermeo@gmail.com <juliancermeo@gmail.com>
2018-01-02 21:07:19 -0500

    New changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FilterPipe } from './filter.pipe';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,69 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
+import {
+  MatToolbarModule,
+  MatTabsModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+  MatMenuModule,
+  MatCheckboxModule,
+  MatSliderModule,
+  MatGridListModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatSelectModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { OrderByComponent } from './order-by/order-by.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { FilterPipe } from './filter.pipe'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProductsComponent,
+    SidebarComponent,
+    OrderByComponent,
+    ShoppingCartComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule, 
+    MatCardModule,
+    MatMenuModule,
+    MatCheckboxModule, 
+    MatSliderModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
